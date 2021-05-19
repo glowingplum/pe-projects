efefdc0e9360a91554e905cb44ba23fa8e6408b7
Thomas <metommylee@gmail.com>
2021-05-18 22:58:36 -0700

    created first php

---
+++ intro-to-php-partials/head.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>[[insert title here]]</title>
+	
+	<meta name="description" content="[[insert description]]">
+	<meta property="og-image" content="[[meta image source]]">
+	<link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+	
+
+<!-- CONTINUES TO INDEX.PHP -->
---
+++ intro-to-php-partials/index.php
@@ -0,0 +1,43 @@
+<?php include('head.php'); ?>
+<!-- CONTINUES FROM HEAD.PHP -->
+
+
+<header class="site-header">
+	<div class="inner-column">
+		Logo
+		<nav>
+			<a href="#">Link 1</a>
+			<a href="#">Link 2</a>
+			<a href="#">Link 3</a>
+		</nav>			
+	</div>
+</header>
+
+<main class="page-content">
+	<section class="page-header">
+		<div class="inner-column">
+			<h1>This is a heading</h1>
+		
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi iure explicabo recusandae necessitatibus eos, natus in, expedita unde, omnis nisi totam. Eum minus, excepturi nostrum, laudantium voluptate doloribus debitis cumque.</p>			
+		</div>
+	</section>
+	
+	<section class="main-section">
+		<div class="inner-column">
+			<h2>About Content</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quaerat, totam sit culpa. Eligendi, eum porro maiores perferendis omnis provident ad ullam nisi, illum eveniet dolor molestias ipsam voluptatum, ut.</p>
+
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum obcaecati eligendi magni! Quod pariatur obcaecati, assumenda laborum beatae est quibusdam, quo optio dicta ipsa unde sed excepturi, illum perspiciatis! Molestias.</p>		
+		</div>
+	</section>
+
+	<section class="secondary-section">
+		<div class="inner-column">
+			<h2>Contact Content</h2>
+			<p>Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Tenetur unde at molestiae harum animi. Velit nostrum praesentium fugiat, culpa, accusantium repudiandae possimus adipisci aut assumenda, unde tenetur optio ullam minus!</p>
+		</div>	
+	</section>
+
+
+<!-- CONTINUES TO FOOTER.PHP -->
+<?php include('footer.php'); ?>
