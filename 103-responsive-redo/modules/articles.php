46a16a9bfe7d3522ac2410fb7b4f480724b02b46
Thomas <metommylee@gmail.com>
2021-06-17 16:14:35 -0700

    Redid responsive layout using PHP

---
+++ 103-responsive-redo/modules/articles.php
@@ -0,0 +1,13 @@
+					<text-wrapper>
+						
+						<h2>This is an "article grid" module. This is its heading.</h2>
+
+						<p>Again, we totally just made that up. It's a box with boxes in it.</p>
+
+					</text-wrapper>
+
+					<section class="articles-grid">
+
+						<?php include('articles-grid.php')?>
+						
+					</section>
