963e3ad3de879a17b9bf77fae8c16fa17ac14bda
Thomas <metommylee@gmail.com>
2021-10-05 15:59:37 -0700

    created project 301-car-site

---
+++ 301-car-site/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>[[insert title]]</title>
+	<meta name="description" content="[[insert description]]">
+
+	<meta property="og:title" content="[[insert title]]" />
+	<meta property="og:description" content="[[insert description]]" />
+	<meta property="og:image" content="[[meta image source]]" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+
+	<link rel="stylesheet" href="styles/main.css" type='text/css'>
+</head>
+
+
+<body>
+	hellloooo
+</body>
+</html>
