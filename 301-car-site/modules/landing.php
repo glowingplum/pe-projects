2b32951404d27b8ed2143d5b23daa2b31a7f5844
Thomas <metommylee@gmail.com>
2021-10-20 18:23:14 -0700

    working on car site

--- 301-car-site/modules/landing.php
+++ 301-car-site/modules/landing.php
@@ -1,4 +1,4 @@
 <landing-content>
-        <h1 class="strong-voice">Hello! This is a&nbsp;diptych</h1>
-        <p class="introduction-voice">You can tell whatever you want. It's a module that has an image and some supporting text. On the smaller screen it works nicely to stack. On a larger screen, it fits side by&nbsp;side.</p>
+        <h1 class="strong-voice"><span>Hello! This is a&nbsp;diptych</span></h1>
+        <p class="introduction-voice"><span>You can tell whatever you want. It's a module that has an image and some supporting text. On the smaller screen it works nicely to stack. On a larger screen, it fits side by&nbsp;side.</span></p>
 </landing-content>
